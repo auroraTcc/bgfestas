513e1a4da7c7d51a0836b6298d9801d1bf8d2269
Fillip Mangia <fillipgms@gmail.com>
2024-09-15 20:26:50 -0300

    display errors

--- controllers/processPedido.php
+++ controllers/processPedido.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     require "../config/conexao.php";
     require "../models/pedido.php";
     require "../models/cliente.php";
