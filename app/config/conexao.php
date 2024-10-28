3e5280aa5771c1bdf27124c07791192adc590c32
Fillip Mangia <fillipgms@gmail.com>
2024-10-28 20:52:12 -0300

    connection

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "ifhostgru_bgfestas";
-$password = "ifspgru@2024";
-$databasename = "ifhostgru_bgfestas";
+$username = "hostdeprojetos_bgfestas";
+$password = "ifspgru@2022";
+$databasename = "hostdeprojetos_bgfestas";
 
 //criação da conexão
 $conn = new mysqli($servername, $username, $password, $databasename);
