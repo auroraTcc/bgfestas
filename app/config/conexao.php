e46e5d9c76b652b209efae62de022f87309cc1a8
Fillip Mangia <fillipgms@gmail.com>
2024-11-22 15:26:23 -0300

    connection fix

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -4,9 +4,9 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $servername = "localhost";
-$username = "hostdeprojetos_bgfestas";
-$password = "ifspgru@2022";
-$databasename = "hostdeprojetos_bgfestas";
+$username = "ifhostgru_bgfestas";
+$password = "ifspgru@2024";
+$databasename = "ifhostgru_bgfestas";
 
 //criação da conexão
 $conn = new mysqli($servername, $username, $password, $databasename);
