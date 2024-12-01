0380e33072e9dcba6e5dc671cee2230fbf66e4fd
Fillip Mangia <fillipgms@gmail.com>
2024-12-01 18:26:37 -0300

    fb name typo

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$databasename = "gfestas";
+$databasename = "bgfestas";
 //criação da conexão
 $conn = new mysqli($servername, $username, $password, $databasename);
 
