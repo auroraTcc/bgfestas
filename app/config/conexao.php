db1e8666ea30e8fe34ca16cedbfc67e6e82bb8dd
Fillip Mangia <fillipgms@gmail.com>
2024-12-04 11:25:43 -0300

    linked local with deploy DB

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$databasename = "bgfestas";
+$servername = "144.217.39.54";
+$username = "ifhostgru_bgfestas";
+$password = "ifspgru@2024";
+$databasename = "ifhostgru_bgfestas";
 //criação da conexão
 $conn = new mysqli($servername, $username, $password, $databasename);
 
 // verificando a conexão
 if (!$conn){
-    //die("conexão falhou".mysqli_connect_error());
-    echo "não foi possível conectar ao banco de dados";
+    die("conexão falhou".mysqli_connect_error());
+    // echo "não foi possível conectar ao banco de dados";
 };
