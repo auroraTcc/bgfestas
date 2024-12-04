ab9e9fafca91b9a3da37f53c3602c3f524e7e23f
Fillip Mangia <fillipgms@gmail.com>
2024-12-04 11:44:38 -0300

    link ajust

--- app/view/admin/login/index.php
+++ app/view/admin/login/index.php
@@ -325,7 +325,7 @@
                 }
 
                 $.ajax({
-                    url: "<?=$isLocal ? "/bgfestas/" : "/"?>controllers/processAlterarSenha.php",
+                    url: "<?=$isLocal ? "/bgfestas/" : "/"?>controllers/processAlterarSenha",
                     type: "POST",
                     dataType: "json",
                     data: {
@@ -361,7 +361,7 @@
             
 
                 $.ajax({
-                    url: "<?=$isLocal ? "/bgfestas/" : "/"?>controllers/processForgotPassword.php",
+                    url: "<?=$isLocal ? "/bgfestas/" : "/"?>controllers/processForgotPassword",
                     type: "POST",
                     data: $(this).serialize(),
                     success: function (response) {
